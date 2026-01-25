b3c3aeb4779afe6486521cf753fa7c01a20c57cf
Maxwell Sithiphong <Csithiphonggg@gmail.com>
2026-01-24 20:07:18 -0800

    Moved Camera button to dashboard

--- beachechoes/app/StartScreen.js
+++ beachechoes/app/StartScreen.js
@@ -27,12 +27,7 @@ export default function StartScreen() {
       >
         Sign Up
       </Button>
-      <Button
-        mode="outlined"
-        onPress={() => router.push("/Camera")}
-      >
-        Camera
-      </Button>
+      
         
     </Background>
   )
