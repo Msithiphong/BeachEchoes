4477fb10354d3e560b13d6130adabb18ca8654c6
Maxwell Sithiphong <Csithiphonggg@gmail.com>
2026-01-29 16:10:03 -0800

    Created more Unit Tests

--- beachechoes/app/(tabs)/Leaderboard.js
+++ beachechoes/app/(tabs)/Leaderboard.js
@@ -3,7 +3,7 @@ import Header from '../../components/Header'
 import Background from '../../components/Background'
 import { useRouter } from 'expo-router'
 
-
+// Jose's File
 export default function Leaderboard() {
     const router = useRouter()
     return(
--- beachechoes/app/index.js
+++ beachechoes/app/index.js
@@ -1,1 +1,2 @@
+// This index.js file shows React Native what the starting screen will be
 export { default } from './StartScreen'
